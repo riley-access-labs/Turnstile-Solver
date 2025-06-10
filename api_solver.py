798d83cf5700748f5e2ff405ebc644b95cf34d3b
Riley Webb <rileywebb@Rileys-MacBook-Pro.local>
2025-06-11 02:35:36 +1000

    Updates

--- api_solver.py
+++ api_solver.py
@@ -145,7 +145,8 @@ class TurnstileAPIServer:
         if self.browser_type in ['chromium', 'chrome', 'msedge']:
             playwright = await async_playwright().start()
         elif self.browser_type == "camoufox":
-            camoufox = AsyncCamoufox(headless=self.headless, geoip=True)
+            headlessMode = 'virtual' if self.headless else False
+            camoufox = AsyncCamoufox(headless=headlessMode, geoip=True, humanize=True)
 
         for _ in range(self.thread_count):
             if self.browser_type in ['chromium', 'chrome', 'msedge']:
